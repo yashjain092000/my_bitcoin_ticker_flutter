2d0ae68aa5a8d115f917b99d4286765e421d4baa
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-09 12:04:45 -0500

    enclose that card in a padding widget.

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -14,11 +14,14 @@ class _PriceScreenState extends State<PriceScreen> {
       ),
         body: Column(
           children: <Widget>[
-            Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.lightBlueAccent,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
               ),
             )
           ],
