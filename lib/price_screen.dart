bd21cc49deccc34bf6d1ee070f5d198422f78d17
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-09 11:54:40 -0500

    create a new file price_screen with a stateful widget PriceScreen() and pass it to home of main.dart.

---
+++ lib/price_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PriceScreen extends StatefulWidget {
+  @override
+  _PriceScreenState createState() => _PriceScreenState();
+}
+
+class _PriceScreenState extends State<PriceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
