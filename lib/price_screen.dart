42bab046e164dce34fc89c5d95edd9fdb7775cd5
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-06-09 12:07:59 -0500

    enclose the Text() in a padding widget.

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -9,9 +9,9 @@ class _PriceScreenState extends State<PriceScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
-      ),
+        appBar: AppBar(
+          title: Text('🤑 Coin Ticker'),
+        ),
         body: Column(
           children: <Widget>[
             Padding(
@@ -23,7 +23,18 @@ class _PriceScreenState extends State<PriceScreen> {
                   borderRadius: BorderRadius.circular(10.0),
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+              child: Text(
+                '1 BTC = ? USD',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ],
         ));
   }
